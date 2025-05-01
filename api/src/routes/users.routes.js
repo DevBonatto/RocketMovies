ab90674075b36fd12a093dc6ee684f29b4815580
Lucca Bonatto <devbonatto@gmail.com>
2025-05-01 19:52:32 -0300

    :sparkles: feat: adding upload file route

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,10 +1,19 @@
-const { Router } = require('express')
+const { Router, response } = require('express')
+const multer = require('multer')
+const uploadConfig = require("../configs/upload")
+
 const UsersController = require('../controllers/UsersController')
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersRouter = Router()
+const upload = multer(uploadConfig.MULTER)
 
 usersRouter.post("/", UsersController.create)
 usersRouter.put("/", ensureAuthenticated, UsersController.update)
+
+usersRouter.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+  console.log(req.file.filename)
+  res.json()
+})
 
 module.exports = usersRouter
