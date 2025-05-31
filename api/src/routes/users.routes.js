1d02bd80773d1f2f562eaee66faa27f42500c01f
Lucca Bonatto <devbonatto@gmail.com>
2025-05-31 19:15:28 -0300

    :sparkles: feat: finishing avatar upload section

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -3,6 +3,7 @@ const multer = require('multer')
 const uploadConfig = require("../configs/upload")
 
 const UsersController = require('../controllers/UsersController')
+const UserAvatarController = require('../controllers/UserAvatarController')
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersRouter = Router()
@@ -11,9 +12,6 @@ const upload = multer(uploadConfig.MULTER)
 usersRouter.post("/", UsersController.create)
 usersRouter.put("/", ensureAuthenticated, UsersController.update)
 
-usersRouter.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
-  console.log(req.file.filename)
-  res.json()
-})
+usersRouter.patch("/avatar", ensureAuthenticated, upload.single("avatar"), UserAvatarController.update)
 
 module.exports = usersRouter
--- api/src/server.js
+++ api/src/server.js
@@ -5,10 +5,14 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const uploadConfig = require("./configs/upload")
+
 const routes = require("./routes")
 
 app.use(express.json())
 app.use(routes) 
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(( error, request, response, next ) => {
   if(error instanceof AppError){
