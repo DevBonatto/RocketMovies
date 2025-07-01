7a53c96461ae334a763642affb15910da932e122
Lucca Bonatto <devbonatto@gmail.com>
2025-07-01 18:37:39 -0300

    :sparkles: feat: adding local storage on token and user

--- frontend/src/hooks/auth.jsx
+++ frontend/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState, useEffect } from "react"
 import { api } from '../services/api'
 
 export const AuthContext = createContext({})
@@ -12,7 +12,9 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password })
       const { user, token } = response.data
 
-      api.defaults.headers.authorization = `Bearer ${token}`
+      localStorage.setItem("@rocketmovies:user", JSON.stringify(user))
+      localStorage.setItem("@rocketmovies:token", token)
+
       setData({ user, token })
 
     } catch (error) {
@@ -23,6 +25,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketmovies:token")
+    const user = localStorage.getItem("@rocketmovies:user")
+
+    if(token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, [])
 
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
